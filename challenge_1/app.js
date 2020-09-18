0f38b26fa1b3c662282b435dda0ab479c0051b48
Claire Melbourne <claire.melbourne@gmail.com>
2020-09-18 17:54:15 -0500

    implement reset button

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -1,5 +1,4 @@
-
-//EVENT HANDLING
+  //CHOOSE A SQUARE
 console.log("here we are together")
 var nextClick = "X";
 var lastClick = "";
@@ -12,7 +11,7 @@ function changeValue(id) {
     // debugger;
     square.innerHTML = "O";
     nextClick = "X";
-    lastClick = "Y";
+    lastClick = "O";
   } else {
     // debugger;
     square.innerHTML = "X";
@@ -23,31 +22,37 @@ function changeValue(id) {
   //debugger;
   saveValue(id, checkForWin);
 }
-
-//KEEPING SCORE (use previously declared nextClick value)
+//BOARD
 var board = [
   ["", "", ""],
   ["", "", ""],
   ["", "", ""]
 ]
+  //CLEAR BOARD
+var clearBoard = () => {
+  board = [
+    ["", "", ""],
+    ["", "", ""],
+    ["", "", ""]
+  ]
+  var squares = document.getElementsByClassName("gridItem");
+  for (var i = 0; i < squares.length; i++){
+    console.log(squares[i].innerHTML);
+    squares[i].innerHTML = "am I your next move?"
+  }
+};
+//KEEPING SCORE (use previously declared nextClick value)
 var saveValue = (id, callback) => {
-  //parse id to object
   id = id.replace('r', '"r"').replace('c', '"c"');
-  //id = id.replace('c', '"c"');
   var coord = JSON.parse(id);
-  //set value at r equal to rowInd
-  //set value at c to colInd
   var rowInd = coord.r;
   var colInd = coord.c;
-  //set board[rowInd][colInd] to lastClick value
   board[rowInd][colInd] = lastClick;
   callback(rowInd, colInd);
   console.log("value saved to board")
 }
 
 var checkForWin = (rowInd, colInd) => {
-  //debugger;
-  //check row
   var rowStreak = 0;
   var colStreak = 0
   for (var i = 0; i < board[rowInd].length; i++) {
@@ -63,32 +68,29 @@ var checkForWin = (rowInd, colInd) => {
   if (rowStreak === 3 || colStreak === 3 ) {
     alert(`${lastClick} is the winner!!`);
   }
-  //loop through array at board[rowInd]
-    //check to see if all values equal lastClick
-  //check column
-  //loop through board
-    //check to see if all values at board[i][colInd] equal last clicked
-  //check diags
-    if(board[1][1] === lastClick) {
-      if ((board[0][0] === lastClick && board[2][2] === lastClick) || (board[2][0] === lastClick && board[0][2] === lastClick)){
-        alert(`${lastClick} is the winner!!`)
-      }
+  if(board[1][1] === lastClick) {
+    if ((board[0][0] === lastClick && board[2][2] === lastClick) || (board[2][0] === lastClick && board[0][2] === lastClick)){
+      alert(`${lastClick} is the winner!!`)
     }
+  }
 }
 
+//EVENT LISTENERS
 //Row 1/Index 0
 document.getElementById("{r:0, c:0}").addEventListener("click", () => {changeValue("{r:0, c:0}")}, false);
-document.getElementById("{r:0, c:1}").addEventListener("click", function() {changeValue("{r:0, c:1}")}, false);
-document.getElementById("{r:0, c:2}").addEventListener("click", function() {changeValue("{r:0, c:2}")}, false);
+document.getElementById("{r:0, c:1}").addEventListener("click", () => {changeValue("{r:0, c:1}")}, false);
+document.getElementById("{r:0, c:2}").addEventListener("click", () => {changeValue("{r:0, c:2}")}, false);
 //Row 2/Index 1
-document.getElementById("{r:1, c:0}").addEventListener("click", function() {changeValue("{r:1, c:0}")}, false);
-document.getElementById("{r:1, c:1}").addEventListener("click", function() {changeValue("{r:1, c:1}")}, false);
-document.getElementById("{r:1, c:2}").addEventListener("click", function() {changeValue("{r:1, c:2}")}, false);
+document.getElementById("{r:1, c:0}").addEventListener("click", () => {changeValue("{r:1, c:0}")}, false);
+document.getElementById("{r:1, c:1}").addEventListener("click", () => {changeValue("{r:1, c:1}")}, false);
+document.getElementById("{r:1, c:2}").addEventListener("click", () => {changeValue("{r:1, c:2}")}, false);
 //Row 3/Index 2
-document.getElementById("{r:2, c:0}").addEventListener("click", function() {changeValue("{r:2, c:0}")}, false);
-document.getElementById("{r:2, c:1}").addEventListener("click", function() {changeValue("{r:2, c:1}")}, false);
-document.getElementById("{r:2, c:2}").addEventListener("click", function() {changeValue("{r:2, c:2}")}, false);
+document.getElementById("{r:2, c:0}").addEventListener("click", () => {changeValue("{r:2, c:0}")}, false);
+document.getElementById("{r:2, c:1}").addEventListener("click", () => {changeValue("{r:2, c:1}")}, false);
+document.getElementById("{r:2, c:2}").addEventListener("click", () => {changeValue("{r:2, c:2}")}, false);
 
+//Button
+document.getElementById("reset").addEventListener("click", () => {clearBoard()})
 
 
 
