eb1c69e385641919d2be96b36edd4956b5399fd3
Claire Melbourne <claire.melbourne@gmail.com>
2020-09-18 16:18:52 -0500

    implement hardcoded v of board and function to save clicked values

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -1,20 +1,23 @@
 
-
+//EVENT HANDLING
 console.log("here we are together")
-var lastClick = ""
+var nextClick = "X";
+var lastClick = "";
 function changeValue(id) {
   var square = document.getElementById(id);
   if(square.innerHTML === "X" || square.innerHTML === "O") {
     // debugger;
     return;
-  } else if (lastClick === "X") {
+  } else if (nextClick === "O") {
     // debugger;
     square.innerHTML = "O";
-    lastClick = "O"
+    nextClick = "X";
+    var lastClick = "Y";
   } else {
     // debugger;
     square.innerHTML = "X";
-    lastClick = "X"
+    nextClick = "O"
+    var lastClick = "X";
   }
   console.log("clicked");
 }
@@ -30,3 +33,26 @@ document.getElementById("{r:1, c:2}").addEventListener("click", function() {chan
 document.getElementById("{r:2, c:0}").addEventListener("click", function() {changeValue("{r:2, c:0}")}, false);
 document.getElementById("{r:2, c:1}").addEventListener("click", function() {changeValue("{r:2, c:1}")}, false);
 document.getElementById("{r:2, c:2}").addEventListener("click", function() {changeValue("{r:2, c:2}")}, false);
+
+//KEEPING SCORE (use previously declared lastClick value)
+
+//save values to array
+var board = [
+  ["", "", ""],
+  ["", "", ""],
+  ["", "", ""]
+]
+var saveValue = (id) => {
+  //parse id to object
+  debugger;
+  var coord = JSON.parse(id);
+  //set value at r equal to rowInd
+  //set value at c to colInd
+  var rowInd = coord.r;
+  var colInd = coord.c;
+  //set board[rowInd][colInd] to lastClick value
+  board[rowInd][colInd] = nextClick;
+  console.log("value saved to board")
+}
+var test ='{"r": 0, "c": 0}'
+console.log(saveValue(test));
