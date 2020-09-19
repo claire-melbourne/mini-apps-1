de7b5da0b3e23d5dc160464d2c787d6acebc0112
Claire Melbourne <claire.melbourne@gmail.com>
2020-09-19 18:49:02 -0500

    apply helper functions to formatCV to generate full doc

--- challenge_2/server.js
+++ challenge_2/server.js
@@ -16,14 +16,54 @@ app.post('/form', (req, res) => {
 
 //format function
 const formatCSV = (jsonObj) => {
-  var object = (jsonObj.jsonSub);
+  var object = (JSON.parse(jsonObj.jsonSub));
   console.log(object);
-
+  var headers = findKeys(object);
+  var linesArray = setValues(headers, object);
+  var fullDoc = (headers.join(',') + '\n' + linesArray.join('\n'))
+  //var fullDoc = headerString + '\n' +
+  console.log('result of fulldoc \n', fullDoc);
+  //var linesArray = setValues(findKeys(object), object);
+  //console.log(linesArray);
 }
-  //iterate over keys
-    //add key to new set
-    //if key value is an array iterate over array performing iteration over keys on each element
-  //var object = JSON.parse(string)
-  //console.log(object, "<----new object")
+
+
+var findKeys = function(object) {
+  var columnNames = {};
+  var recurse = function(obj) {
+    for (var key in obj) {
+      if (key === "children") {
+        for (var i = 0; i < obj.children.length; i++) {
+          findKeys(obj.children[i]);
+        }
+      } else {
+        columnNames[key] = true;
+      }
+    }
+  }
+  recurse(object);
+  return Object.keys(columnNames);
+}
+
+var setValues = function(headers, obj) {
+  var linesArray = [];
+  var recurse = function(obj) {
+    var line = [];
+    for (var i = 0; i < headers.length; i++) {
+      if (!headers[i]) {
+        line.push('NA');
+      } else {
+      line.push(obj[headers[i]]);
+      }
+    }
+    var lineString = line.join(',');
+    linesArray.push(lineString);
+    for (var j = 0; j < obj.children.length; j++) {
+      recurse(obj.children[j]);
+    }
+  }
+  recurse(obj);
+  return linesArray;
+}
 
 app.listen(PORT, () => {console.log(`listening at port ${PORT}`)});
